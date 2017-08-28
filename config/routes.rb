e2126d9e09fdd8569b8cafab8996fcb3c719e666
Victor Allen <vamuigua@gmail.com>
2017-08-28 10:08:38 +0300

    Added todos and items routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #setting todos and nesting items RESTful routes
+  resources :todos do
+    resources :items
+  end
 end
